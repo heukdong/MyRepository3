47582fac7b0c9f23152a99411fb00935ecb55d89
김동우 <@DESKTOP-E9CTHB5>
2020-06-17 18:41:12 +0900

    new

--- MyGit/src/b.java
+++ MyGit/src/b.java
@@ -1,9 +1,9 @@
 
 public class b {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public b() {
+		System.out.println("b constructor");
+		
 	}
 
 }
--- MyGit/src/c.java
+++ MyGit/src/c.java
@@ -1,9 +1,8 @@
 
 public class c {
 
-	public static void main(String[] args) {
-		System.out.println("Hi git");
-		System.out.println("Hello~");
+	public c() {
+		System.out.println("c constructor");
 
 	}
 
